bdd67e1199cb14d5a64f429ab852d19026fa639c
jdr <37280273+JOSEDAVIDRODRIGUEZ2018@users.noreply.github.com>
2020-09-20 02:03:26 -0500

    implementacion de conexion a firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:healtychallengeapp/ui/Login/ChatHealtyChallenge.dart';
 import 'package:healtychallengeapp/ui/Login/login.dart';
 import 'package:healtychallengeapp/ui/Login/registration.dart';
 import 'package:healtychallengeapp/ui/Login/welcome.dart';
 
-void main()=>runApp(HealtyChallengeApp());
+void main() => runApp(HealtyChallengeApp());
 
 class HealtyChallengeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-initialRoute: Welcome.id ,
+      initialRoute: Welcome.id,
       routes: {
-        Welcome.id: (context) =>Welcome(),
+        Welcome.id: (context) => Welcome(),
         Login.id: (context) => Login(),
-        Registration.id: (context)=>Registration(),
-
+        Registration.id: (context) => Registration(),
+        ChatHealtyChallenge.id:(context)=>ChatHealtyChallenge(),
       },
     );
   }
--- lib/ui/Login/ChatHealtyChallenge.dart
+++ lib/ui/Login/ChatHealtyChallenge.dart
@@ -1,35 +1,41 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatHealtyChallenge extends StatefulWidget {
-
   static const String id = 'chat_healtychallenge';
   @override
   _ChatHealtyChallengeState createState() => _ChatHealtyChallengeState();
 }
 
-
 class _ChatHealtyChallengeState extends State<ChatHealtyChallenge> {
-  final _auth =FirebaseAuth.instance;
+
+  // ignore: deprecated_member_use
+  final _firestore = Firestore.instance;
+  final _auth = FirebaseAuth.instance;
+
+  // ignore: deprecated_member_use
   FirebaseUser loggedInUser;
+  String messageText;
 
   @override
   void initState() {
     super.initState();
     getCurrentUser();
   }
-  void getCurrentUser()async{
-    try{
-      final user= await _auth.currentUser;
-      if(user!=null){
-        loggedInUser=user;
+
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser;
+      if (user != null) {
+        loggedInUser = user;
         print(loggedInUser.email);
       }
-    }catch(e){
+    } catch (e) {
       print(e);
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,6 +45,8 @@ class _ChatHealtyChallengeState extends State<ChatHealtyChallenge> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
+                _auth.signOut();
+                Navigator.pop(context);
                 //Implement logout functionality
               }),
         ],
@@ -51,25 +59,30 @@ class _ChatHealtyChallengeState extends State<ChatHealtyChallenge> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
-            // decoration: kMessageContainerDecoration,
+              // decoration: kMessageContainerDecoration,
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
-                //      decoration: kMessageTextFieldDecoration,
+                      //      decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   FlatButton(
                     onPressed: () {
+                      print(loggedInUser.email);
+                   /*   _firestore.collection('mensajes').add({
+                        'email': loggedInUser.email,
+                        'text': messageText,
+                      });*/
                       //Implement send functionality.
                     },
                     child: Text(
                       'Send',
-                //      style: kSendButtonTextStyle,
+                      //      style: kSendButtonTextStyle,
                     ),
                   ),
                 ],
